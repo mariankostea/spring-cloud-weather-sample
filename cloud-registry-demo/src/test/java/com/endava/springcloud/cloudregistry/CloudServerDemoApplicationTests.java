d4aa35006f84c6c72b4da7c76c3ab331947f2b7f
maconstantin <constantin.marian@endava.com>
2019-11-25 09:33:23 +0200

    Renamed registry service application

--- cloud-registry-demo/src/test/java/com/endava/springcloud/cloudregistry/CloudServerDemoApplicationTests.java
+++ cloud-registry-demo/src/test/java/com/endava/springcloud/cloudregistry/CloudServerDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.endava.springcloud.cloudserverdemo;
+package com.endava.springcloud.cloudregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
